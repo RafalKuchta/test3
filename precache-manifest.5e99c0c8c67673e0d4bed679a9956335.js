1c9828d100f33a52217864186bbd57f0a07f79aa
Rafał Kuchta <rafalmax12@interia.pl>
2019-03-23 17:01:17 +0100

    add build

---
+++ precache-manifest.5e99c0c8c67673e0d4bed679a9956335.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca8789d6bb6dac684f74",
+    "url": "/todoapp/static/js/runtime~main.ca8789d6.js"
+  },
+  {
+    "revision": "9a3d9e54e306d5cd8c0e",
+    "url": "/todoapp/static/js/main.9a3d9e54.chunk.js"
+  },
+  {
+    "revision": "b756de0126c5fe6c0161",
+    "url": "/todoapp/static/js/1.b756de01.chunk.js"
+  },
+  {
+    "revision": "9a3d9e54e306d5cd8c0e",
+    "url": "/todoapp/static/css/main.f3708c2b.chunk.css"
+  },
+  {
+    "revision": "fe3c9f12f84d4947b1e55f4adbb8a46d",
+    "url": "/todoapp/index.html"
+  }
+];
